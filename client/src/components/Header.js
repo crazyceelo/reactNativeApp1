17cbcdd0c23dedd9216491f05ba05c5db9711de7
Carlos Perez <solracias@gmail.com>
2018-09-05 20:55:41 -0700

    header now has background color

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 const Header = () => {
-  return <Text>Albums</Text>;
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>Albums</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: "#F8F8F8"
+  }
+});
 
 export default Header;
