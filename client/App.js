efac63e7d724cb54ebde1b5ae7c02c1ee4f5c0a2
Carlos Perez <solracias@gmail.com>
2018-09-05 21:27:31 -0700

    reusable header component is complete

--- client/App.js
+++ client/App.js
@@ -7,7 +7,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View>
-        <Header />
+        <Header headerText="Albums" />
       </View>
     );
   }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 
-const Header = () => {
+const Header = props => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Albums</Text>
+      <Text style={styles.textStyle}>{props.headerText}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 20
+    fontSize: 20,
+    justifyContent: "center",
+    textAlign: "center",
+    height: 60,
+    paddingTop: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: "relative"
   },
   viewStyle: {
     backgroundColor: "#F8F8F8"
