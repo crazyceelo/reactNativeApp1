f9ef5dbf83f814e7d87ea8299fc1e896ef5a0135
Carlos Perez <solracias@gmail.com>
2018-09-07 13:18:21 -0700

    now renders album lists with a unique key using axios

--- client/src/components/AlbumList.js
+++ client/src/components/AlbumList.js
@@ -3,18 +3,38 @@ import { View, Text } from "react-native";
 import axios from "axios";
 
 class AlbumList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      albums: []
+    };
+  }
   componentWillMount() {
     axios
       .get("https://rallycoding.herokuapp.com/api/music_albums")
-      .then(response => console.log(response));
+      .then(response =>
+        this.setState({
+          albums: response.data
+        })
+      );
   }
 
+  renderAlbums() {
+    return this.state.albums.map((album, index) => (
+      <Text key={index}>{album.title}</Text>
+    ));
+  }
+
+  // {profile.skills.slice(0, 4).map((skill, index) => (
+  //   <li key={index} className="list-group-item">
+  //     <i className="fa fa-check pr-1" />
+  //     {skill}
+  //   </li>
+  // ))}
+
   render() {
-    return (
-      <View>
-        <Text>Album listsss</Text>
-      </View>
-    );
+    console.log(this.state.albums);
+    return <View>{this.renderAlbums()}</View>;
   }
 }
 
